53f0c89fe13f140959b95f8f8cabba8243f18ebf
Dario <dario.favaron@gmail.com>
2023-12-07 17:31:09 +0100

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,6 +95,6 @@ if st.button("Get Tasks"):
               # get all pages we have access to with the integration
               pages = get_all_pages(headers)
 
-              st.success(pages, height=200)
+              st.json(pages, expanded=False)
         except Exception as e:
             st.exception(f"An error occurred: {e}")
