430da69d412bc0ab38ef586a994ad9d78d0890f8
Dario <dario.favaron@gmail.com>
2023-12-07 22:20:31 +0100

    update

--- data_handle.py
+++ data_handle.py
@@ -1,10 +1,7 @@
-import pandas as pd
-
-def visualize_notion_db_properties(db_response):
+def visualize_notion_db_properties_v2(db_response):
     """
-    Takes the JSON response from a Notion API query for a database,
-    and formats the relevant details into a DataFrame for visualization,
-    including properties like 'relation', 'rollup', 'status', and 'date'.
+    Improved version of the function to visualize Notion database properties.
+    This version includes handling for 'status' and 'rollup' property types.
     """
     try:
         properties = db_response["properties"]
@@ -17,12 +14,13 @@ def visualize_notion_db_properties(db_response):
 
             # For 'select' and 'status' types, extract options
             if prop_type in ["select", "status"]:
-                if 'options' in prop_details:
-                    options = " - ".join([f"{opt['name']}, {opt['color']}" for opt in prop_details["options"]])
+                if 'options' in prop_details.get(prop_type, {}):
+                    options = " - ".join([f"{opt['name']}, {opt['color']}"
+                                          for opt in prop_details[prop_type]["options"]])
 
             # For 'relation' and 'rollup' types, extract related database ID
             if prop_type in ["relation", "rollup"]:
-                related_db_id = prop_details["relation"]["database_id"]
+                related_db_id = prop_details[prop_type]["database_id"]
 
             # Additional property type handling can be added here if needed
 
