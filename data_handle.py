decc98a67ad52c55fabd30e45946fc18d8c99bc6
Dario <dario.favaron@gmail.com>
2023-12-07 22:04:24 +0100

    update

--- data_handle.py
+++ data_handle.py
@@ -6,27 +6,32 @@ def visualize_notion_db_properties(db_response):
     and formats the relevant details into a DataFrame for visualization,
     including properties like 'relation', 'rollup', 'status', and 'date'.
     """
-    properties = db_response["properties"]
-    data = []
+    try:
+        properties = db_response["properties"]
+        data = []
 
-    for prop_name, prop_details in properties.items():
-        prop_type = prop_details["type"]
-        options = ""
-        related_db_id = ""
+        for prop_name, prop_details in properties.items():
+            prop_type = prop_details["type"]
+            options = ""
+            related_db_id = ""
 
-        # For 'select' and 'status' types, extract options
-        if prop_type in ["select", "status"]:
-            if 'options' in prop_details[prop_type]:
-                options = " - ".join([f"{opt['name']}, {opt['color']}" for opt in prop_details[prop_type]["options"]])
+            # For 'select' and 'status' types, extract options
+            if prop_type in ["select", "status"]:
+                if 'options' in prop_details[prop_type]:
+                    options = " - ".join([f"{opt['name']}, {opt['color']}" for opt in prop_details[prop_type]["options"]])
 
-        # For 'relation' and 'rollup' types, extract related database ID
-        if prop_type in ["relation", "rollup"]:
-            related_db_id = prop_details[prop_type]["database_id"]
+            # For 'relation' and 'rollup' types, extract related database ID
+            if prop_type in ["relation", "rollup"]:
+                related_db_id = prop_details[prop_type]["database_id"]
 
-        # Additional property type handling can be added here if needed
+            # Additional property type handling can be added here if needed
 
-        data.append([prop_name, prop_type, options, related_db_id])
+            data.append([prop_name, prop_type, options, related_db_id])
 
-    # Create a DataFrame
-    df = pd.DataFrame(data, columns=["Property", "Type", "Options", "Related Database ID"])
-    return df
+        # Create a DataFrame
+        df = pd.DataFrame(data, columns=["Property", "Type", "Options", "Related Database ID"])
+        return df
+
+    except KeyError as e:
+        print(f"Error: {e} key not found in the JSON response.")
+        return None
