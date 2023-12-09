eb1ec12a5d545d5c8e3493ad4d561f19ab3e0fa8
Dario <dario.favaron@gmail.com>
2023-12-09 05:08:51 +0100

    update

--- langchain_agents.py
+++ langchain_agents.py
@@ -8,7 +8,7 @@ from API.PineconeAPI import PineconeAPI  # Replace 'your_pinecone_api_module' wi
 # LangChain agent for Notion Query Tool
 class NotionQueryAgent(BaseLLM):
     def __init__(self, api_key, db_id):
-        self.notion_api = NotionAPI(api_key)
+        self.notion_api = api_key
         self.db_id = db_id
 
     def complete(self, rows_amount):
@@ -19,48 +19,48 @@ class NotionQueryAgent(BaseLLM):
 # LangChain agent for Notion Retrieve Tool
 class NotionRetrieveAgent(BaseLLM):
     def __init__(self, api_key, db_id):
-        self.notion_api = NotionAPI(api_key)
+        self.notion_api = api_key
         self.db_id = db_id
     # Implementation similar to NotionQueryAgent with specific logic for retrieving a single row
 
 # LangChain agent for Notion Update Tool
 class NotionUpdateAgent(BaseLLM):
     def __init__(self, api_key, db_id):
-        self.notion_api = NotionAPI(api_key)
+        self.notion_api = api_key
         self.db_id = db_id
     # Implementation similar to NotionQueryAgent with specific logic for updating a Notion database row
 
 # LangChain agent for OpenAI Embed Text
 class OpenAIEmbedTextAgent(BaseLLM):
     def __init__(self, api_key, db_id):
-        self.notion_api = NotionAPI(api_key)
+        self.notion_api = api_key
         self.db_id = db_id
     # Implementation for generating embeddings for a given text
 
 # LangChain agent for OpenAI Embed Database Line
 class OpenAIEmbedDatabaseLineAgent(BaseLLM):
     def __init__(self, api_key, db_id):
-        self.notion_api = NotionAPI(api_key)
+        self.notion_api = api_key
         self.db_id = db_id
     # Implementation for generating embeddings for a single line of a database
 
 # LangChain agent for Pinecone Load Data
 class PineconeLoadDataAgent(BaseLLM):
     def __init__(self, api_key, db_id):
-        self.notion_api = NotionAPI(api_key)
+        self.notion_api = api_key
         self.db_id = db_id
     # Implementation for loading data into the Pinecone database
 
 # LangChain agent for Pinecone Request Similarity Docs
 class PineconeRequestSimilarityDocsAgent(BaseLLM):
     def __init__(self, api_key, db_id):
-        self.notion_api = NotionAPI(api_key)
+        self.notion_api = api_key
         self.db_id = db_id
     # Implementation for requesting documents from Pinecone DB based on similarity
 
 # LangChain agent for OpenAI Check Duplicates
 class OpenAICheckDuplicatesAgent(BaseLLM):
     def __init__(self, api_key, db_id):
-        self.notion_api = NotionAPI(api_key)
+        self.notion_api = api_key
         self.db_id = db_id
     # Implementation for checking duplicates in text using OpenAI
