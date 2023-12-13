962acd576c812975083af6d12146b6aae1e7057f
Dario <dario.favaron@gmail.com>
2023-12-13 12:12:19 +0100

    Update query method in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -236,7 +236,7 @@ if st.button("Button 2 - Get one element from Note Inbox"):
 
                     st.json(input_notes_vectors[0], expanded=False)
 
-                    areas_response = pineconeClass.query(input_notes_vectors[0], 10, "areas")
+                    areas_response = pineconeClass.query(query_vector=input_notes_vectors[0]["values"], topK=10, namespace="areas")
                     projects_response = pineconeClass.query(input_notes_vectors[0], 10, "projects")
                     tasks_response = pineconeClass.query(input_notes_vectors[0], 10, "tasks")
 
