90f1e660fb898c092e8a0e292271c2a25e429dd3
Dario <dario.favaron@gmail.com>
2023-12-18 00:13:13 +0100

    Update st.json() method to use .json() on page_content

--- streamlit_app.py
+++ streamlit_app.py
@@ -217,7 +217,7 @@ if st.button("Button 2 - Get one element from Note Inbox, embed it, and extract 
 
                 page_content = notionClass.get_page_content(inbox_note_to_review["id"])
                 st.write("page_content: ")
-                st.json(page_content, expanded=False)
+                st.json(page_content.json(), expanded=False)
                 
                 st.write("visualize_notion_database_row_object: ")
 
