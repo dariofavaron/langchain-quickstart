cbc5bde4c1ca4256a87ec2609e61609c32dbf422
Dario <dario.favaron@gmail.com>
2023-12-30 20:09:21 +0100

    Add st parameter to create_task_table function

--- GeneralFunctions/CreateTaskDataframe.py
+++ GeneralFunctions/CreateTaskDataframe.py
@@ -1,12 +1,26 @@
 import pandas as pd
 import json
 
-def create_task_table(area_json, project_json, task_json):
+def create_task_table(st, area_json, project_json, task_json):
+    """
+    Create a task table based on area, project, and task JSON data.
 
+    Args:
+        area_json (dict): JSON data for areas.
+        project_json (dict): JSON data for projects.
+        task_json (dict): JSON data for tasks.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the task table.
+    """
     # Extracting information
-    area_info = {item["id"]: item["properties"]["Name"]["title"][0]["plain_text"] for item in area_json["results"]}
-    project_info = {item["id"]: item["properties"]["Name"]["title"][0]["plain_text"] for item in project_json["results"]}
-    task_info = [(item["id"], item["properties"]["Name"]["title"][0]["plain_text"]) for item in task_json["results"]]
+    area_info = {item["id"]: item["properties"]["Name"]["title"][0]["plain_text"]
+                    for item in area_json["results"]}
+    st.write(area_info)
+    project_info = {item["id"]: item["properties"]["Name"]["title"][0]["plain_text"]
+                    for item in project_json["results"]}
+    task_info = [(item["id"], item["properties"]["Name"]["title"][0]["plain_text"])
+                    for item in task_json["results"]]
 
     # Create a list to hold our final data
     final_data = []
@@ -14,11 +28,13 @@ def create_task_table(area_json, project_json, task_json):
     # Matching task IDs with corresponding project and area IDs
     for task_id, task_name in task_info:
         # Find project related to the task
-        project_id = next((relation["id"] for relation in task_json["results"] if relation["id"] == task_id), None)
+        project_id = next((relation["id"] for relation in task_json["results"]
+                        if relation["id"] == task_id), None)
         project_name = project_info.get(project_id, "Unknown")
 
         # Find area related to the project
-        area_id = next((relation["id"] for relation in project_json["results"] if relation["id"] == project_id), None)
+        area_id = next((relation["id"] for relation in project_json["results"]
+                        if relation["id"] == project_id), None)
         area_name = area_info.get(area_id, "Unknown")
 
         # Append to the final data
@@ -27,3 +43,4 @@ def create_task_table(area_json, project_json, task_json):
     # Create DataFrame
     df = pd.DataFrame(final_data, columns=["Task Name", "Project Related", "Area Related"])
     return df
+
