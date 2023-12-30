c42a7a383632d4b3259121031e56f23c2ccb5d47
Dario <dario.favaron@gmail.com>
2023-12-30 20:21:31 +0100

    Refactor task_info to use dictionary instead of list

--- GeneralFunctions/CreateTaskDataframe.py
+++ GeneralFunctions/CreateTaskDataframe.py
@@ -19,8 +19,8 @@ def create_task_table(st, area_json, project_json, task_json):
     st.write(area_info)
     project_info = {item["id"]: item["properties"]["Name"]["title"][0]["plain_text"]
                     for item in project_json["results"]}
-    task_info = [(item["id"], item["properties"]["Name"]["title"][0]["plain_text"])
-                    for item in task_json["results"]]
+    task_info = {item["id"]: item["properties"]["Name"]["title"][0]["plain_text"]
+                    for item in task_json["results"]}
 
     # Create a list to hold our final data
     final_data = []
@@ -28,13 +28,13 @@ def create_task_table(st, area_json, project_json, task_json):
     # Matching task IDs with corresponding project and area IDs
     for task_id, task_name in task_info:
         # Find project related to the task
-        project_id = next((relation["id"] for relation in task_json["results"]
-                        if relation["id"] == task_id), None)
+        project_id = next((relation["properties"]["Projects"]["relation"][0]["id"] for relation in task_json["results"]
+                        if relation["properties"]["Projects"]["relation"][0]["id"] == task_id), None)
         project_name = project_info.get(project_id, "Unknown")
 
         # Find area related to the project
-        area_id = next((relation["id"] for relation in project_json["results"]
-                        if relation["id"] == project_id), None)
+        area_id = next((relation["properties"]["Areas"]["relation"][0]["id"] for relation in project_json["results"]
+                        if relation["properties"]["Areas"]["relation"][0]["id"] == project_id), None)
         area_name = area_info.get(area_id, "Unknown")
 
         # Append to the final data
