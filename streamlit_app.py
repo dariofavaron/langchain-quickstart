1eaf4834d5f207e05583ffeeae1e744cc1665d11
Dario <dario.favaron@gmail.com>
2024-01-08 02:09:16 +0100

    Add note analysis functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,13 +72,16 @@ def new_task_draft():
             #the note inbox to be analyzed are the one tagged "New". 
             st.write("st.session_state.notes_dataframe: ")
             st.dataframe(st.session_state.notes_dataframe)
+            st.json(st.session_state.notes_dataframe.to_json(), expanded=False)
             st.write(st.session_state.notes_dataframe.loc[st.session_state.notes_dataframe["Note Status"] == "New"] )
+
             #extract a note from the note dataframe
-            st.session_state.note_in_analysis = st.session_state.notes_dataframe.iloc[0]
+            #st.session_state.note_in_analysis = st.session_state.notes_dataframe.iloc[0]
 
             #extract the first note with status "New" from the note dataframe
-            #st.session_state.note_in_analysis = st.session_state.notes_dataframe[st.session_state.notes_dataframe["Note Name"] == "New"].iloc[0]
-
+            st.session_state.note_in_analysis = st.session_state.notes_dataframe[st.session_state.notes_dataframe["Note Status"] == "New"].iloc[0]
+            st.write("st.session_state.note_in_analysis: ")
+            st.write(st.session_state.note_in_analysis)
 
             st.write("- Notion data retrieved and dataframes created successfully")
 
