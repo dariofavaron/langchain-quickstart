e602a4a8384e98925cb19d6fdb87ae2ca7d5895d
Dario <dario.favaron@gmail.com>
2024-01-08 02:00:58 +0100

    Refactor code to filter notes_dataframe by 'note_status' column

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,12 +72,12 @@ def new_task_draft():
             #the note inbox to be analyzed are the one tagged "New". 
             st.write("st.session_state.notes_dataframe: ")
             st.dataframe(st.session_state.notes_dataframe)
-            st.json(st.session_state.notes_dataframe, expanded=False)
+            st.write(st.session_state.notes_dataframe.loc[st.session_state.notes_dataframe['note_status'] == "new"] )
             #extract a note from the note dataframe
-            st.session_state.note_in_analysis= st.session_state.notes_dataframe.iloc[0]
+            st.session_state.note_in_analysis = st.session_state.notes_dataframe.iloc[0]
 
             #extract the first note with status "New" from the note dataframe
-            #st.session_state.note_in_analysis= st.session_state.notes_dataframe[st.session_state.notes_dataframe["Note Name"] == "New"].iloc[0]
+            #st.session_state.note_in_analysis = st.session_state.notes_dataframe[st.session_state.notes_dataframe["Note Name"] == "New"].iloc[0]
 
 
             st.write("- Notion data retrieved and dataframes created successfully")
